c98c8c9ee908a3da4a2974636e349b0a01d69d24
Mateus Lucas <mateus.some.ms@gmail.com>
2025-02-18 13:16:11 -0300

    criado os useStates para o input titulo, para o input quantidade de paginas e para manter a lista e adicionar os novos item quando eu clicar no botão adicionar! feita a logica para o botão adicionar os livros na lista

--- mateusHookProject/src/App.tsx
+++ mateusHookProject/src/App.tsx
@@ -2,20 +2,37 @@ import { useState } from "react";
 
 import Header from "./assets/componentes/header";
 import Footer from "./assets/componentes/footer";
-// import Book from "./assets/componentes/book";
-// import Title from "./assets/componentes/title";
+import Book from "./assets/componentes/book";
+import Title from "./assets/componentes/title";
 // import { bestMovie, booksList } from "./data";
 import Button from "./assets/componentes/button";
+import { booksList } from "./data";
 
 // import interstellarSrc from "./assets/interstellarSrc.jpg";
 
 import "./App.css";
 
 function App() {
-  const [counter, setCounter] = useState(0);
+  const [bookTitle, setBookTitle] = useState("");
+  const [booksPages, setBooksPages] = useState(0);
+  const [books, setBooks] = useState(booksList);
+
+  function handleNameChange(event: React.ChangeEvent<HTMLInputElement>) {
+    setBookTitle(event.target.value);
+  }
+
+  function handlePagesChange(event: React.ChangeEvent<HTMLInputElement>) {
+    setBooksPages(event.target.valueAsNumber);
+  }
 
   function handleClick() {
-    setCounter(counter + 1);
+    const newBook = {
+      title: bookTitle,
+      pages: booksPages,
+      isRead: false,
+      isFavorite: false,
+    };
+    setBooks([...books, newBook]);
   }
 
   // function handleClick(messageText: string) {
@@ -33,48 +50,51 @@ function App() {
   return (
     <div className="app">
       <Header />
+
       <div className="container">
-        <Button onClick={handleClick}> {counter}</Button>
-      </div>
-      {/* <div className="container">
-        <Title>
+        <Title>Meus Livros:</Title>
+        {/* <p>
           <s>Livros emprestados </s>Meus Livros:
-        </Title>
-        <p>
-          <s>Livros emprestados </s>Meus Livros:
-        </p>
+        </p> */}
         <ul className="books-list">
-          {booksList.map((book) => (
+          {books.map((book) => (
             <Book book={book} key={book.title} />
           ))}
         </ul>
-        <Title>{bestMovie}</Title>
+        {/* <Title>{bestMovie}</Title>
         <div className="images-container">
           <img src={interstellarSrc} alt="Poster 1 interstellar" />
           <img src="/interstellarPublic.jpg" alt="Poster 2 interstellar" />
+          </div> */}
+        <div className="books-form">
+          <input
+            placeholder="Título"
+            type="text"
+            value={bookTitle}
+            onChange={handleNameChange}
+          />
+          <input
+            placeholder="Quantidade de Paginas"
+            type="number"
+            value={booksPages}
+            onChange={handlePagesChange}
+          />
+          <Button onClick={handleClick}>
+            <strong>Adicionar</strong>
+          </Button>
         </div>
-        <Button onClick={() => handleClick("Esse filme é o melhor!")}>
-          <strong>Esse filme é bom?</strong>
-        </Button>
 
-        <Button onClick={() => handleClick("Não, Ele é muito demorado!")}>
+        {/* <Button onClick={() => handleClick("Não, Ele é muito demorado!")}>
           <strong>Posso assistir sem medo?</strong>
-        </Button>
+        </Button> */}
 
-        <input
-          placeholder="Digite aqui"
-          type="text"
-          onChange={(event) => {
-            handleChange(event);
-          }}
-        />
-        {/* Dá pra fazer o botão das duas formas: */}
+        {/* Dá pra fazer o botão das duas formas:
       {/* <button onClick={handleClick}>Esse filme é bom?</button> */}
-      {/* Da segunda forma não precisa da função lá em cima, porém dependendo da função tem que ver qual compensa! Acho que a de baixo é melhor para quando é um botão bem simples, ai fica um codigo mais limpo. */}
-      {/* <button onClick={() => alert("Esse filme é o melhor!")}>
+        {/* Da segunda forma não precisa da função lá em cima, porém dependendo da função tem que ver qual compensa! Acho que a de baixo é melhor para quando é um botão bem simples, ai fica um codigo mais limpo. */}
+        {/* <button onClick={() => alert("Esse filme é o melhor!")}>
           Esse filme é bom?
         </button> */}
-      {/* </div> */}
+      </div>
       <Footer />
     </div>
   );
