2c2fa6ae66cc652eefdbbf59f751643a3a710fe1
Mateus Lucas <mateus.some.ms@gmail.com>
2025-02-18 11:01:44 -0300

    iniciando novo projeto! vamos começar a implementar o hook do useState e setState com o botão. foi feita mais algumas aterações como inserir imagens do outro projeto e arrumar o cs da pagina

--- mateusHookProject/src/App.tsx
+++ mateusHookProject/src/App.tsx
@@ -2,32 +2,37 @@ import { useState } from "react";
 
 import Header from "./assets/componentes/header";
 import Footer from "./assets/componentes/footer";
-import Book from "./assets/componentes/book";
-import Title from "./assets/componentes/title";
-import { bestMovie, booksList } from "./data";
+// import Book from "./assets/componentes/book";
+// import Title from "./assets/componentes/title";
+// import { bestMovie, booksList } from "./data";
 import Button from "./assets/componentes/button";
 
-import interstellarSrc from "./assets/interstellarSrc.jpg";
+// import interstellarSrc from "./assets/interstellarSrc.jpg";
 
 import "./App.css";
 
 function App() {
-  function handleClick(messageText: string) {
-    alert(messageText);
-  }
+  function handleClick() {}
 
-  function handleChange(event: React.ChangeEvent<HTMLInputElement>) {
-    console.log(event.target.value);
-  }
-  // vai receber um array de numeros e vai retornar ou outro array com os numeros multiplicados por 2
-  function dubleNumber(numberList: Array<number>) {
-    return numberList.map((number) => number * 2);
-  }
+  // function handleClick(messageText: string) {
+  //   alert(messageText);
+  // }
+
+  // function handleChange(event: React.ChangeEvent<HTMLInputElement>) {
+  //   console.log(event.target.value);
+  // }
+  // // vai receber um array de numeros e vai retornar ou outro array com os numeros multiplicados por 2
+  // function dubleNumber(numberList: Array<number>) {
+  //   return numberList.map((number) => number * 2);
+  // }
 
   return (
     <div className="app">
       <Header />
       <div className="container">
+        <Button onClick={handleClick}> Botão 1</Button>
+      </div>
+      {/* <div className="container">
         <Title>
           <s>Livros emprestados </s>Meus Livros:
         </Title>
@@ -60,12 +65,12 @@ function App() {
           }}
         />
         {/* Dá pra fazer o botão das duas formas: */}
-        {/* <button onClick={handleClick}>Esse filme é bom?</button> */}
-        {/* Da segunda forma não precisa da função lá em cima, porém dependendo da função tem que ver qual compensa! Acho que a de baixo é melhor para quando é um botão bem simples, ai fica um codigo mais limpo. */}
-        {/* <button onClick={() => alert("Esse filme é o melhor!")}>
+      {/* <button onClick={handleClick}>Esse filme é bom?</button> */}
+      {/* Da segunda forma não precisa da função lá em cima, porém dependendo da função tem que ver qual compensa! Acho que a de baixo é melhor para quando é um botão bem simples, ai fica um codigo mais limpo. */}
+      {/* <button onClick={() => alert("Esse filme é o melhor!")}>
           Esse filme é bom?
         </button> */}
-      </div>
+      {/* </div> */}
       <Footer />
     </div>
   );
